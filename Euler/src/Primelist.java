8139fec47a0b47365fc821654891c62dd0ccc377
Zerwas <Zerwas@Zerwas-PC>
2014-09-14 16:34:56 +0200

    upperbound added hope it still works

--- Euler/src/Primelist.java
+++ Euler/src/Primelist.java
@@ -2,16 +2,15 @@ import java.util.Iterator;
 
 /**
  * 
- * @author zerwas
- * calculates primes
+ * @author zerwas calculates primes
  */
-public class Primelist implements Iterable<Integer>{
+public class Primelist implements Iterable<Integer> {
 
 	int maxnumber;
 	boolean[] numbers;
 
 	void erase(int x) {
-		//TODO handle 3 separately for better performance
+		// TODO handle 3 separately for better performance
 		for (int i = x * x; i < maxnumber && x <= Math.sqrt(maxnumber); i += x << 1)// x=x+prime*2,da
 																					// x+prime
 																					// gerade
@@ -68,13 +67,14 @@ public class Primelist implements Iterable<Integer>{
 	public Primelist(int max) {
 		maxnumber = max;
 		numbers = new boolean[maxnumber];
-		numbers[0]=true;
-		numbers[1]=true;
+		numbers[0] = true;
+		numbers[1] = true;
 		for (int g = 4; g < maxnumber; g += 2)// erase for 2
 		{
 			numbers[g] = true;
 		}
-		for (int i = 3; i < maxnumber; i += 2) {
+		int upperBound = (int) Math.sqrt(maxnumber) + 1;
+		for (int i = 3; i < upperBound; i += 2) {
 			if (!numbers[i]) {
 				erase(i);
 			}
@@ -85,35 +85,39 @@ public class Primelist implements Iterable<Integer>{
 	public Iterator<Integer> iterator() {
 		return new PrimeIterator();
 	}
+
 	/**
 	 * 
-	 * @author zerwas
-	 * iterator that returns all the primes under maxnumber
+	 * @author zerwas iterator that returns all the primes under maxnumber
 	 */
-	private class PrimeIterator implements Iterator<Integer>{
+	private class PrimeIterator implements Iterator<Integer> {
 
-		int i=0;
-		public PrimeIterator(){
+		int i = 0;
+
+		public PrimeIterator() {
 			next();
 		}
+
 		@Override
 		public boolean hasNext() {
-			return i!=-1;
+			return i != -1;
 		}
 
 		@Override
 		public Integer next() {
-			int x=i++;
-			while (i<maxnumber&&isNotPrime(i)) i++;
-			if (i==maxnumber) i=-1;
+			int x = i++;
+			while (i < maxnumber && isNotPrime(i))
+				i++;
+			if (i == maxnumber)
+				i = -1;
 			return x;
 		}
 
 		@Override
 		public void remove() {
 			throw new UnsupportedOperationException();
-			
+
 		}
-		
+
 	}
 }
