060b50e9338c1a1ad2eeab8876f9e9e4edd11288
Zerwas <f_starke@gmx.de>
2014-09-14 21:11:20 +0200

    tests for primelist added primelist now uses first element of array

--- Euler/src/Primelist.java
+++ Euler/src/Primelist.java
@@ -6,20 +6,13 @@ import java.util.Iterator;
  */
 public class Primelist implements Iterable<Integer> {
 
-	int maxnumber;
-	boolean[] numbers;
+	private int maxnumber;
+	private boolean[] numbers;
 
-	void erase(int x) {
+	private void erase(int x) {
 		// TODO handle 3 separately for better performance
-		for (int i = x * x; i < maxnumber && x <= Math.sqrt(maxnumber); i += x << 1)// x=x+prime*2,da
-																					// x+prime
-																					// gerade
-																					// und
-																					// schon
-																					// bei
-																					// erase
-																					// 2
-																					// abgehandelt
+		for (int i = (x * x)-1; i < maxnumber; i += x << 1)
+		// x=x+prime*2,da x+prime gerade und schon bei erase 2 abgehandelt
 		{
 			numbers[i] = true;
 		}
@@ -31,7 +24,7 @@ public class Primelist implements Iterable<Integer> {
 	 * @return true iff i is not prime
 	 */
 	public boolean isNotPrime(int i) {
-		return numbers[i];
+		return numbers[i-1];
 	}
 
 	/**
@@ -40,7 +33,7 @@ public class Primelist implements Iterable<Integer> {
 	 * @return true iff i is prime
 	 */
 	public boolean isPrime(int i) {
-		return !numbers[i];
+		return !numbers[i-1];
 	}
 
 	/**
@@ -63,19 +56,21 @@ public class Primelist implements Iterable<Integer> {
 		}
 		return erg;
 	}
-
+/**
+ * 
+ * @param max array length--> max number represented is max-1
+ */
 	public Primelist(int max) {
 		maxnumber = max;
 		numbers = new boolean[maxnumber];
-		numbers[0] = true;
-		numbers[1] = true;
-		for (int g = 4; g < maxnumber; g += 2)// erase for 2
+		numbers[0] = true;//1 is not prime
+		for (int g = 3; g < maxnumber; g += 2)// erase for 2
 		{
 			numbers[g] = true;
 		}
 		int upperBound = (int) Math.sqrt(maxnumber) + 1;
 		for (int i = 3; i < upperBound; i += 2) {
-			if (!numbers[i]) {
+			if (isPrime(i)) {
 				erase(i);
 			}
 		}
@@ -105,8 +100,8 @@ public class Primelist implements Iterable<Integer> {
 
 		@Override
 		public Integer next() {
-			int x = i++;
-			while (i < maxnumber && isNotPrime(i))
+			int x = ++i;
+			while (i < maxnumber && numbers[i])
 				i++;
 			if (i == maxnumber)
 				i = -1;
@@ -118,6 +113,5 @@ public class Primelist implements Iterable<Integer> {
 			throw new UnsupportedOperationException();
 
 		}
-
 	}
 }
---
+++ Euler/src/Test_Primelist.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class Test_Primelist extends Test_Primelist_Basic{
+
+	protected Primelist p2, pmax;
+
+
+	@Test
+	public void testmax() {
+		p2 = new Primelist(Integer.MAX_VALUE / 2);
+		pmax = new Primelist((Integer.MAX_VALUE/4)*3);
+		for (int i = 1; i < Integer.MAX_VALUE / 2; i++) {
+			assertFalse("Difference at number " + i,
+					p2.isPrime(i) ^ pmax.isPrime(i));
+		}
+	}
+
+}
